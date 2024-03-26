6cbd7cdb63f3775c150bac9f455d18eec3d7faa8
Matheus Auret <teeuzudo@gmail.com>
2024-03-26 18:22:38 -0300

    V1 project

---
+++ scripts/commands.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# O shell irá encerrar a execução do script quando um comando falhar
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    echo " Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
+    sleep 0.1
+done
+
+echo " Postgres Database Start Successfull ($POSTGRES_HOST $POSTGRES_PORT)"
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
